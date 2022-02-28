a934c7669456b65eb7bf2f1625eeff3440fee776
Dansereau21 <93531652+Dansereau21@users.noreply.github.com>
2022-02-28 09:55:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+* File: ButtonPanel.java
+* Author: Nagy János
+* Copyright: 2021, Nagy János
+* Group: Szoft V
+* Date: 2021-09-03
+* Github: https://github.com/Dansereau21
+* Licenc: GNU GPL
+*/
+
+var oldalIn= document.getElememtById("oldal");
+var szamitGomb = document.getElementById("szamitGomb");
+var eredmenyIn = document.getElementById("eredmény");
+
+szamitGomb.addEventListener('click' , function(){
+    let oldal= oldalIn.value;
+    let alfaszog =alfaszogIn.value;
+    if(oldalStr.match(mInta))(
+        console.log("ok")
+    )else
+       console.log("nem ok")
+
+       
+}
